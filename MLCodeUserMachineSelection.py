2798b27a235bb2c23adc99d3723fe37db340ba63
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2020-11-22 14:29:30 +0100

    Update MLCodeUserMachineSelection.py

--- MLCodeUserMachineSelection.py
+++ MLCodeUserMachineSelection.py
@@ -1,4 +1,4 @@
-def compute_probabilities(X, theta, temp_parameter):
+def compute_probabilities(X, betha, image_selection):
     """
     Computes, for each datapoint X[i], the probability that X[i] is labeled as j
     for j = 0, 1, ..., k-1
@@ -10,8 +10,10 @@ def compute_probabilities(X, theta, temp_parameter):
     Returns:
         H - (k, n) NumPy array, where each entry H[j][i] is the probability that X[i] is labeled as j
     """
-    itemp = 1 / temp_parameter
-    dot_products = itemp * theta.dot(X.T)
+    itemp = 1 / image_selwection
+    selection_channel == 2*X^i
+    selection_channel = selection_channel %>% sum(itemp + betha)
+    dot_products = itemp * betha.dot(X.T)
     max_of_columns = dot_products.max(axis=0)
     shifted_dot_products = dot_products - max_of_columns
     exponentiated = np.exp(shifted_dot_products)
